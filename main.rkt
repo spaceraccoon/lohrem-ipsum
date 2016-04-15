b858506b4c3b8ddb67068441dab600c262fdc6f8
limzhiweieugene <limzhiweieugene@gmail.com>
2016-04-15 12:19:24 -0400

    Update main.rkt
    Added paragraph and capitalization functionality.

--- main.rkt
+++ main.rkt
@@ -7,11 +7,33 @@
   (response/xexpr
    `(html (head (title "Lohrem Ipsum"))
           (body (h1 "Lohrem Ipsum")
-                ,(render-words)))))
- 
+                ,(render-text 3)))))
+
+(define (render-text n)
+  `(div ((class "lol"))
+        ,@(render-paragraphs n)))
+
+(define (render-paragraphs n)
+  (if (= n 0)
+      '()
+      (cons (render-paragraph) (render-paragraphs (- n 1)))))
+
+(define (render-paragraph)
+  `(p ,(generate-paragraph (random 15 25))))
+
+(define (capitalize str)
+  (string-set! str 0 (char-upcase (string-ref str 0)))
+  str)
+
+(define (generate-paragraph n)
+  (let ([sentence (capitalize (generate-sentence (random 5 10) singlish-dictionary))])
+    (if (= n 1)
+        sentence
+        (string-append sentence " " (generate-paragraph (- n 1))))))
+      
 (define (render-words)
-  `(div ((class "result"))
-        ,@(select-words 3 singlish-dictionary)))
+  `(div ((class "sentence"))
+        ,@(string-titlecase (generate-sentence 3 singlish-dictionary))))
 
 (define singlish-dictionary
   (list
@@ -19,11 +41,18 @@
    "lah"
    "leh"
    "walao"
+   "sian"
+   "aiyo"
+   "bodoh"
+   "kambing"
+   "sayang"
+   "liao"
+   "liddat"
+   "blur"
    ))
 
-(define (select-words n lst)
-  (cond
-    [(= n 0) '(".")]
-    [(= n 1) (cons (list-ref lst (random (length lst))) (select-words (- n 1) lst))]
-    [else (append (cons (list-ref lst (random (length lst))) '(" ")) (select-words (- n 1) lst))]))
+(define (generate-sentence n lst)
+  (if (= n 1)
+      (string-append (list-ref lst (random (length lst))) ".")
+      (string-append (list-ref lst (random (length lst))) " " (generate-sentence (- n 1) lst))))
         
